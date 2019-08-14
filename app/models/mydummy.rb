cef93760bbb41eaf13c495f11917278b157ce7e6
Lior Kedem <lior.kdm@gmail.com>
2019-08-14 22:34:38 +0300

    commit from source tree

---
+++ app/models/mydummy.rb
@@ -0,0 +1,2 @@
+class Mydummy < ApplicationRecord
+end
---
+++ test/models/mydummy_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MydummyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
